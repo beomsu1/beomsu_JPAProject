561174f56e4cb90888679a4f210b7db5fa21bc70
Beomsu <beumsu1221@gmail.com>
2023-07-12 01:28:18 +0900

    commit

---
+++ src/main/java/org/bs/x2/X2Application.java
@@ -0,0 +1,13 @@
+package org.bs.x2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class X2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(X2Application.class, args);
+	}
+
+}
