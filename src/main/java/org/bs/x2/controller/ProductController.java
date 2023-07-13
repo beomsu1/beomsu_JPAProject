73138bbf2ed412bc1f555b910d9ef61e72f3a9a9
Beomsu <beumsu1221@gmail.com>
2023-07-14 01:29:14 +0900

    파일업로드

--- src/main/java/org/bs/x2/controller/ProductController.java
+++ src/main/java/org/bs/x2/controller/ProductController.java
@@ -1,11 +1,18 @@
 package org.bs.x2.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.bs.x2.dto.PageRequestDTO;
 import org.bs.x2.dto.PageResponseDTO;
+import org.bs.x2.dto.ProductDTO;
 import org.bs.x2.dto.ProductListDTO;
 import org.bs.x2.service.ProductService;
+import org.bs.x2.util.FileUploader;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +28,9 @@ public class ProductController {
 
     private final ProductService service;
 
+    private final FileUploader uploader;
+
+    // 목록
     @GetMapping("list")
     public PageResponseDTO<ProductListDTO> list (PageRequestDTO requestDTO){
 
@@ -28,5 +38,17 @@ public class ProductController {
 
     }
 
+    // 등록
+    @PostMapping("")
+    public Map<String,Long> register (ProductDTO productDTO){
+
+        log.info(productDTO);
+
+        // 파일 등록
+        List<String> fileNames = 
+        uploader.uploadFiles(productDTO.getFiles());
+
+        return Map.of("result" , 123L);
+    }
     
 }
